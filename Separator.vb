38e9a1d147ff52d1d111310653a6fa61f48f0953
hon <38807226+hon4@users.noreply.github.com>
2024-09-29 18:50:25 +0300

    Update Separator.vb

--- Separator.vb
+++ Separator.vb
@@ -1,17 +1,21 @@
 Public Class Separator
 
-    Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
-        MyBase.OnPaint(e)
+    Private darkPanel As Panel
 
+    Public Sub New()
+        InitializeComponent()
         MyBase.Height = 2
         MyBase.BackColor = Color.White
-        Dim dark As New Panel
-        With dark
-            .Height = 1
-            .BackColor = Color.FromArgb(160, 160, 160)
-            .Dock = DockStyle.Top
 
-        End With
-        MyBase.Controls.Add(dark)
+        darkPanel = New Panel With {
+            .Height = 1,
+            .BackColor = Color.FromArgb(160, 160, 160),
+            .Dock = DockStyle.Top
+        }
+        MyBase.Controls.Add(darkPanel)
+    End Sub
+
+    Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
+        MyBase.OnPaint(e)
     End Sub
 End Class
